2ff9c92b705ff4f897b57f4d436b65db0b7c8766
Bosco Lau <bosco.kc.lau@gmail.com>
2021-04-12 20:26:40 -0700

    Tests: create_master_key.

--- encryptionlib_tests.py
+++ encryptionlib_tests.py
@@ -10,6 +10,8 @@ KEY_LENGTH = "key_length"
 HMAC_HASH = "hmac_hash"
 KDF = "kdf"
 KEY = "KEY"
+
+# Expects
 NONE_EXCEPTION_CLASS_EXPECT = None
 NONE_EX_MSG = None
 
@@ -22,7 +24,7 @@ BASE_KEY = "632c2812e46d4604102ba7618e9d6d7d2f8128f6266b4a03264d2a0460b7dcb3"
 ########################################################################################################################
 class CreateKeyWithPbkdf2(unittest.TestCase):
     def setUp(self):
-        print("Test:", self.__class__.__name__, ":", self._testMethodName)
+        print("Test:", self.__class__.__name__ + "." + self._testMethodName)
 
     def test_happy_path(self):
         data = {SECRET: "password", SALT: "salt", ITERATIONS: 1000, KEY_LENGTH: 32, HMAC_HASH: "sha256"}
@@ -36,6 +38,9 @@ class CreateKeyWithPbkdf2(unittest.TestCase):
 
         self.verify_create_key_with_pbkdf2(data, expect, ValueError, "Unsupported hmac_hash: sha384")
 
+    ############################
+    ## Verifiers
+    ############################
     def verify_create_key_with_pbkdf2(self, data: dict, expect: dict, exception_class_expect: BaseException,
                                       ex_msg: str):
 
@@ -58,7 +63,7 @@ class CreateKeyWithPbkdf2(unittest.TestCase):
 ########################################################################################################################
 class CreateKey(unittest.TestCase):
     def setUp(self):
-        print("Test:", self.__class__.__name__, ":", self._testMethodName)
+        print("Test:", self.__class__.__name__ + "." + self._testMethodName)
 
     def test_happy_path(self):
         data = {SECRET: "password", SALT: "salt", ITERATIONS: 1000, KEY_LENGTH: 32, HMAC_HASH: "sha256", KDF: "pbkdf2"}
@@ -78,6 +83,9 @@ class CreateKey(unittest.TestCase):
 
         self.verify_create_key(data, expect, ValueError, "Unsupported kdf: bcrypt")
 
+    ############################
+    ## Verifiers
+    ############################
     def verify_create_key(self, data: dict, expect: dict, exception_class_expect: BaseException, ex_msg: str):
 
         try:
@@ -94,5 +102,52 @@ class CreateKey(unittest.TestCase):
             print(str(be))
 
 
+########################################################################################################################
+## Tests: create_master_key
+########################################################################################################################
+class CreateMasterKey(unittest.TestCase):
+    def setUp(self):
+        print("Test:", self.__class__.__name__ + "." + self._testMethodName)
+
+    def test_happy_path(self):
+        data = {SECRET: "password", SALT: "salt", ITERATIONS: 1000, KEY_LENGTH: 32, HMAC_HASH: "sha256", KDF: "pbkdf2"}
+        expect = {KEY: BASE_KEY}
+
+        self.verify_create_master_key(data, expect, NONE_EXCEPTION_CLASS_EXPECT, NONE_EX_MSG)
+
+    def test_unsupported_hash(self):
+        data = {SECRET: "password", SALT: "salt", ITERATIONS: 1000, KEY_LENGTH: 32, HMAC_HASH: "sha384", KDF: "pbkdf2"}
+        expect = {KEY: BASE_KEY}
+
+        self.verify_create_master_key(data, expect, ValueError, "Unsupported hmac_hash: sha384")
+
+    def test_unsupported_kdf(self):
+        data = {SECRET: "password", SALT: "salt", ITERATIONS: 1000, KEY_LENGTH: 32, HMAC_HASH: "sha256", KDF: "bcrypt"}
+        expect = {KEY: BASE_KEY}
+
+        self.verify_create_master_key(data, expect, ValueError, "Unsupported kdf: bcrypt")
+
+    ############################
+    ## Verifiers
+    ############################
+    def verify_create_master_key(self, data: dict, expect: dict, exception_class_expect: BaseException, ex_msg: str):
+
+        try:
+            key = create_master_key(data[SECRET], data[SALT], data[ITERATIONS], data[KEY_LENGTH], data[HMAC_HASH],
+                                    data[KDF])
+
+            self.assertEqual(expect[KEY], key)
+
+            if exception_class_expect is not None:
+                self.fail(f"Exception should have been thrown, of class: {str(exception_class_expect)}")
+        except BaseException as be:
+            self.assertIsInstance(be, exception_class_expect)
+            self.assertEqual(ex_msg, str(be))
+            print(str(be))
+
+
+########################################################################################################################
+## Maih
+########################################################################################################################
 if __name__ == '__main__':
     unittest.main()
