4a00460e03c7bad0042319c2fdf983f30c630787
Bosco Lau <bosco.kc.lau@gmail.com>
2021-04-12 20:32:16 -0700

    Revert names to cipherschemes.

--- cipherschemes_tests.py
+++ cipherschemes_tests.py
@@ -5,7 +5,7 @@ Created on Apr 11, 2021
 """
 import unittest
 
-from keycreationlib import *
+from ciphershemes import *
 
 AES128WithSHA256 = "AES128WithSHA256"
 AES128WithSHA512 = "AES128WithSHA512"
